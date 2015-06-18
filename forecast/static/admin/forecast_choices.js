de8c26f93970a6617a0d76e0b405e95e68c414c3
Albert Lee <albertlee@yandex.ru>
2015-06-18 14:35:32 +0300

    hide & show forecast magnitude min max

--- forecast/static/admin/forecast_choices.js
+++ forecast/static/admin/forecast_choices.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     var choices = $('#choices-group');
     var forecastType = $('#id_forecast_type');
+    var range = $('#id_min,#id_max').closest('.row');
 
     function triggerChoices() {
         if($(forecastType).val() == '1') {
@@ -12,6 +13,19 @@ $(document).ready(function () {
 
     triggerChoices();
 
+    function rangeShow() {
+        if ($(forecastType).val() == '3') {
+            range.show()
+        } else {
+            range.hide()
+        }
+    }
+
+    rangeShow();
+
+
     $(forecastType).change(triggerChoices)
+        .on('keyup keydown', function () { $(this).trigger('change') });
+    $(forecastType).change(rangeShow)
         .on('keyup keydown', function () { $(this).trigger('change') });
 });
