e87d39a5da5a193bbf8badc05784163e13acac6d
Yuriy Potapov <yuriypotapov@collabimo.com>
2015-08-07 15:07:42 +0300

    edit icon in profile

--- forecast/templatetags/filters.py
+++ forecast/templatetags/filters.py
@@ -5,9 +5,3 @@ from django.contrib.humanize.templatetags.humanize import intcomma
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
-
-
-# @register.filter
-# def currency(dollars):
-#     dollars = round(float(dollars), 2)
-#     return "$%s%s" % (intcomma(int(dollars)), ("%0.2f" % dollars)[-3:])
