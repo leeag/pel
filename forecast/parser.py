a98e105de87b22c5a65d853e9956e6bf7d136cce
Albert Lee <snowartpeople@gmail.com>
2015-09-15 19:33:55 +0300

    changed field for translation

--- forecast/parser.py
+++ forecast/parser.py
@@ -8,20 +8,28 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'Peleus.settings'
 django.setup()
 
 from forecast.models import Country, Region, City
+from django.utils import translation
 
+translation.activate('en')
 with open('countries_regions_cities.csv') as f:
-    content = f.readlines()[:20]
+    content = f.readlines()
 
-co = Country()
-re = Region()
+
 ci = City()
 
 for row in content:
     cells = row.split(',')
     if cells[0] != '':
-        co.name_en = cells[0]    #Country name adding (Україна, Polska..)
+        co = Country()
+        co.name = cells[0]    #Country name adding (Україна, Polska..)
+        print co.name
         co.save()
-    # elif cells[1] != '':
-    #     re.name_en = cells[1]    #Region name adding (Львівська, Małopolskie ..)
+    elif cells[1] != '':
+        re = Region()
+        country = Country.objects.get(name=cells[0])
+        re.name = cells[1]    #Region name adding (Львівська, Małopolskie ..)
+        re.country = country
+        print re.name
+        re.save()
     # elif cells[4] != '':
-    #     ci.name_en = cells[4]    #City name adding (Львів, Paris ..)
+    #     ci.name = cells[4]    #City name adding (Львів, Paris ..)
